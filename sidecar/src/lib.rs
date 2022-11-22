56d0355fccabdead145337c6d602dcf223ce4d03
Gina Yuan <gina.yuan@gmail.com>
2022-11-22 09:43:04 -0800

    QuACK receiver listens for quACKs on a UDP socket

--- sidecar/src/lib.rs
+++ sidecar/src/lib.rs
@@ -1,4 +1,7 @@
 use quack::*;
+use bincode;
+use tokio;
+use tokio::{sync::mpsc, net::UdpSocket};
 
 pub enum SidecarType {
     QuackSender,
@@ -17,7 +20,15 @@ pub struct Sidecar {
 impl Sidecar {
     /// Create a new sidecar.
     pub fn new(ty: SidecarType, interface: &str, threshold: usize, bits: usize) -> Self {
-        unimplemented!()
+        assert_eq!(bits, 32, "ERROR: <num_bits_id> must be 32");
+        Self {
+            ty,
+            interface: interface.to_string(),
+            threshold,
+            bits,
+            quack: Quack::new(threshold),
+            log: vec![],
+        }
     }
 
     /// Start the raw socket that listens to the specified interface and
@@ -26,23 +37,40 @@ impl Sidecar {
     /// only listens for outgoing packets, and additionally logs the packet
     /// identifiers.
     pub fn start(&self) {
-        unimplemented!()
+        println!("warning: unimplemented");
     }
 
     /// Receive quACKs on the given UDP port. Returns the channel on which
     /// to loop received quACKs.
-    pub fn listen(&self, port: u16) -> tokio::sync::mpsc::Receiver<Quack> {
-        unimplemented!()
+    pub fn listen(&self, port: u16) -> mpsc::Receiver<Quack> {
+        // https://docs.rs/tokio/latest/tokio/sync/mpsc/fn.channel.html
+        // buffer up to 100 messages
+        let (tx, rx) = mpsc::channel(100);
+        let buf_len = {
+            let quack = Quack::new(self.threshold);
+            bincode::serialize(&quack).unwrap().len()
+        };
+        tokio::spawn(async move {
+            let addr = format!("127.0.0.1:{}", port);
+            let socket = UdpSocket::bind(addr).await.unwrap();
+            let mut buf = vec![0; buf_len];
+            let (nbytes, _) = socket.recv_from(&mut buf).await.unwrap();
+            assert_eq!(nbytes, buf.len());
+            // TODO: check that it's actually a quack
+            let quack: Quack = bincode::deserialize(&buf).unwrap();
+            tx.send(quack).await.unwrap();
+        });
+        rx
     }
 
     /// Snapshot the quACK.
     pub fn quack(&self) -> &Quack {
-        unimplemented!()
+        &self.quack
     }
 
     /// Snapshot the quACK and current log.
     pub fn quack_with_log(&self) -> (&Quack, &IdentifierLog) {
-        unimplemented!()
+        (&self.quack, &self.log)
     }
 
     /// Decode the quACK given the current snapshot.
