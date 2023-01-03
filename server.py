f7598a8ea640f11ac4b808d0435ca5e47b6304fa
Gina Yuan <gina.yuan@gmail.com>
2023-01-03 22:24:37 +0000

    proxy nginx requests to a python webserver

---
+++ server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+https://gist.github.com/mdonkers/63e115cc0c79b4f6b8b3a6b797e485c7
+Very simple HTTP server in python for logging requests
+Usage::
+    ./server.py [<port>]
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s", str(self.path), str(self.headers).strip())
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\nBody: (%d bytes)",
+                str(self.path), str(self.headers).strip(), len(post_data))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=8000):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...')
+
+if __name__ == '__main__':
+    run()
+
