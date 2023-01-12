48d17ce0b76644e4c7eac39502e825b2c7548c1d
Gina Yuan <gina.yuan@gmail.com>
2023-01-12 22:52:46 +0000

    log many more curl results, and only use verbose format if "trials" arg is not provided

--- webserver/run_client.sh
+++ webserver/run_client.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$#" -lt 2 ]; then
-    echo -e "Usage:   $0 [n-bytes] [1|2|3] [trials (default: 1)] [stdout]? [stderr]? [cubic|reno]?"
+    echo -e "Usage:   $0 [n-bytes] [1|2|3] [trials]? [stdout]? [stderr]? [cubic|reno]?"
     echo -e "Example: $0 1M 3 1"
     exit 1
 fi
@@ -22,7 +22,7 @@ echo $http
 
 # Parse the number of trials to run
 if [ -z "$3" ]; then
-    trials=1
+    trials=0
 elif [ $3 -lt 1 ]; then
     echo -e "Must run at least 1 trial"
     exit 1
@@ -60,15 +60,15 @@ $cmd > $file
 # Run $trials trials
 echo "Data Size: $1"
 echo "HTTP: $http"
-if [ $trials -eq 1 ]; then
-    fmt='\n\n      time_connect:  %{time_connect}s\n   time_appconnect:  %{time_appconnect}s\ntime_starttransfer:  %{time_starttransfer}s\n                   ----------\n        time_total:  %{time_total}s\n'
+if [ $trials -eq 0 ]; then
+    fmt='\n\n      time_connect:  %{time_connect}s\n   time_appconnect:  %{time_appconnect}s\ntime_starttransfer:  %{time_starttransfer}s\n                   ----------\n        time_total:  %{time_total}s\n\nexitcode: %{exitcode}\nresponse_code: %{response_code}\nsize_upload: %{size_upload}\nsize_download: %{size_download}\nerrormsg: %{errormsg}\n'
     cmd="curl-exp -v $http --insecure $quiche_cc --data-binary @$file https://$addr/ -w \"$fmt\""
     eval $cmd
 else
-    fmt='%{time_connect}\t%{time_appconnect}\t%{time_starttransfer}\t\t%{time_total}\n'
-    cmd="curl-exp $http --insecure $quiche_cc --data-binary @$file https://$addr/ -w \"$fmt\" -o $stdout_file 2>$stderr_file"
+    fmt='%{time_connect}\t%{time_appconnect}\t%{time_starttransfer}\t\t%{time_total}\t%{exitcode}\t\t%{response_code}\t\t%{size_upload}\t\t%{size_download}\t%{errormsg}\n'
+    cmd="curl-exp $http --insecure $quiche_cc --data-binary @$file https://$addr/ -w \"$fmt\" -o $stdout_file 2>>$stderr_file"
     echo $cmd
-    echo -e "\ntime_connect\ttime_appconnect\ttime_starttransfer\ttime_total"
+    echo -e "\ntime_connect\ttime_appconnect\ttime_starttransfer\ttime_total\texitcode\tresponse_code\tsize_upload\tsize_download\terrormsg"
     for i in $(seq 1 1 $trials); do
         eval "$cmd --max-time 300" || true
     done
