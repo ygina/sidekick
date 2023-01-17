93e064acf1508518a0b5211975f228b5c124f56a
Gina Yuan <gina.yuan@gmail.com>
2023-01-17 04:46:51 +0000

    run experiments for missing data points in plot automatically

--- plot/data_size_vs_total_time.py
+++ plot/data_size_vs_total_time.py
@@ -1,12 +1,20 @@
+import argparse
+import subprocess
+import os
+import sys
 import os.path
 import statistics
 from os import path
 from common import *
 
-# DATE = '010922'
-DATE = ''
-NUM_TRIALS = 20
-TARGET_XS = [x for x in range(200, 1000, 200)] + [x for x in range(2000, 40001, 2000)]
+DATE = None
+NUM_TRIALS = None
+MAX_X = None
+EXECUTE = None
+WORKDIR = None
+TARGET_XS = [x for x in range(100, 1001, 100)] + \
+            [x for x in range(1000, 10001, 1000)] + \
+            [x for x in range(10000, 100001, 10000)]
 
 class DataPoint:
     def __init__(self, arr, normalize=None):
@@ -25,11 +33,39 @@ class DataPoint:
         self.avg = statistics.mean(arr)
         self.stdev = None if len(arr) == 1 else statistics.stdev(arr)
 
-def parse_data(filename, normalize, data_key='time_total'):
+def execute_cmd(loss, http_version, cc, trials, data_size):
+    suffix = f'loss{loss}p/{cc}'
+    results_file = f'{WORKDIR}/results/{suffix}/{http_version}.txt'
+    if http_version == 'pep':
+        bm = ['tcp', '--pep']
+    elif http_version == 'tcp-tso':
+        bm = ['tcp', '--tso']
+    elif http_version == 'pep-tso':
+        bm = ['tcp', '--tso', '--pep']
+    elif 'quic-' in http_version:
+        bm = ['quic']
+    else:
+        bm = [http_version]
+    subprocess.Popen(['mkdir', '-p', f'results/{suffix}'], cwd=WORKDIR).wait()
+    cmd = ['sudo', '-E', 'python3', 'mininet/net.py', '--loss2', str(loss),
+        '--benchmark'] + bm + ['-cc', cc, '-t', str(trials),
+        '-n', f'{data_size}k']
+    print(cmd)
+    p = subprocess.Popen(cmd, cwd=WORKDIR, stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    with open(results_file, 'ab') as f:
+        for line in p.stdout:
+            sys.stdout.buffer.write(line)
+            sys.stdout.buffer.flush()
+            f.write(line)
+    p.wait()
+
+def parse_data(loss, cc, http_version, normalize, data_key='time_total'):
     """
     Parses the median keyed time and the data size.
     ([data_size], [time_total])
     """
+    filename = get_filename(loss, cc, http_version)
     with open(filename) as f:
         lines = f.read().split('\n')
     xy_map = {}
@@ -78,26 +114,35 @@ def parse_data(filename, normalize, data_key='time_total'):
     xs = []
     ys = []
     for data_size in xy_map:
-        if data_size in TARGET_XS:
+        if data_size in TARGET_XS and data_size <= MAX_X:
             xs.append(data_size)
     xs.sort()
     if len(xs) != len(TARGET_XS):
         missing_xs = []
         for x in TARGET_XS:
-            if x not in xs:
-                missing_xs.append(x)
-        print(f'missing {len(missing_xs)} xs: {missing_xs}')
+            if x in xs or x > MAX_X:
+                continue
+            missing_xs.append(x)
+        if EXECUTE:
+            for x in missing_xs:
+                execute_cmd(loss, http_version, cc, NUM_TRIALS, x)
+        elif len(missing_xs) > 0:
+            print(f'missing {len(missing_xs)} xs: {missing_xs}')
     try:
         for i in range(len(xs)):
             x = xs[i]
             y = xy_map[x]
             if len(y) < NUM_TRIALS:
                 missing = NUM_TRIALS - len(y)
-                print(f'{x}k missing {missing}/{NUM_TRIALS}')
+                if EXECUTE:
+                    execute_cmd(loss, http_version, cc, missing, x)
+                else:
+                    print(f'{x}k missing {missing}/{NUM_TRIALS}')
             xs[i] /= 1000.
             ys.append(DataPoint(y, normalize=xs[i] if normalize else None))
-    except:
+    except Exception as e:
         import pdb; pdb.set_trace()
+        raise e
     return (xs, ys)
 
 def get_filename(loss, cc, http):
@@ -121,7 +166,7 @@ def plot_graph(loss, cc, pdf,
             print('Path does not exist: {}'.format(filename))
             continue
         try:
-            data[http_version] = parse_data(filename, normalize,
+            data[http_version] = parse_data(loss, cc, http_version, normalize,
                                             data_key=data_key)
         except Exception as e:
             print('Error parsing: {}'.format(filename))
@@ -152,7 +197,56 @@ def plot_graph(loss, cc, pdf,
     if pdf is not None:
         save_pdf(pdf)
 
-for cc in ['cubic']:
-    for loss in [1, 2, 5]:
-        plot_graph(loss=loss, cc=cc, pdf='median_{}_loss{}p.pdf'.format(cc, loss), use_median=True)
-        plot_graph(loss=loss, cc=cc, pdf='mean_{}_loss{}p.pdf'.format(cc, loss), use_median=False)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--execute',
+                        action='store_true',
+                        help='Execute benchmarks for missing data points')
+    parser.add_argument('-t', '--trials',
+                        default=20,
+                        type=int,
+                        help='Number of trials to plot (default: 20)')
+    parser.add_argument('--max-x',
+                        default='50000',
+                        type=int,
+                        help='Maximum x to plot, in kB (default: 40000)')
+    parser.add_argument('--mean',
+                        action='store_true',
+                        help='Plot mean graphs')
+    parser.add_argument('--median',
+                        action='store_true',
+                        help='Plot median graphs')
+    parser.add_argument('--loss',
+                        type=int,
+                        help='Loss percentages to plot [0|1|2|5]. If no '
+                             'argument is provided, plots 1, 2, and 5.')
+    parser.add_argument('--cc',
+                        default='cubic',
+                        help='TCP congestion control algorithm to plot '
+                             '[reno|cubic] (default: cubic)')
+    parser.add_argument('--workdir',
+                        default=os.environ['HOME'] + '/sidecar',
+                        help='Working directory (default: $HOME/sidecar)')
+    parser.add_argument('--date',
+                        default='',
+                        help='Find results at '
+                             '../results/<DATE>/loss<LOSS>p/<CC>/<HTTP>.txt, '
+                             'usually something like 010922 if archived '
+                             '(default: \'\')')
+    args = parser.parse_args()
+
+    if args.loss is None:
+        losses = [1, 2, 5]
+    else:
+        losses = [args.loss]
+    cc = args.cc
+    NUM_TRIALS = args.trials
+    DATE = args.date
+    MAX_X = args.max_x
+    EXECUTE = args.execute
+    WORKDIR = args.workdir
+    for loss in losses:
+        if args.median:
+            plot_graph(loss=loss, cc=cc, pdf='median_{}_loss{}p.pdf'.format(cc, loss), use_median=True)
+        if args.mean:
+            plot_graph(loss=loss, cc=cc, pdf='mean_{}_loss{}p.pdf'.format(cc, loss), use_median=False)
