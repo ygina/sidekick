fea057b0f7fca91de5e526bfdcb6475357c82f20
Gina Yuan <gina.yuan@gmail.com>
2023-01-19 16:01:37 +0000

    quack sender should quack to h2 not h1

--- mininet/net.py
+++ mininet/net.py
@@ -69,7 +69,7 @@ class SidecarNetwork():
         # Start the quACK sender on r1
         sclog('Starting the QUIC sidecar sender on r1...')
         self.r1.cmdPrint(f'RUST_LOG=error ./target/release/sidecar --interface r1-eth1 '+ \
-            f'quack-sender --target-addr 10.0.1.10:5103 ' + \
+            f'quack-sender --target-addr 10.0.2.10:5103 ' + \
             f'--frequency-ms {self.sidecar} &')
 
     def kill_quack_sender(self):
