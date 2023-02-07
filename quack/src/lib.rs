4009d893fbadbc2a9c4b57c08046c9cb30b00996
Gina Yuan <gina.yuan@gmail.com>
2023-02-07 22:03:29 +0000

    quack trait

--- quack/src/lib.rs
+++ quack/src/lib.rs
@@ -7,8 +7,20 @@ pub mod arithmetic {
 
 }
 
-mod quack;
+mod psum;
+mod montgomery;
 mod decoded_quack;
 
-pub use crate::quack::{Quack, PowerSumQuack, Identifier};
+pub use crate::psum::PowerSumQuack;
+pub use crate::montgomery::MontgomeryQuack;
 pub use decoded_quack::{DecodedQuack, IdentifierLog};
+
+pub type Identifier = u32;
+
+pub trait Quack {
+    fn new(threshold: usize) -> Self;
+    fn insert(&mut self, value: Identifier);
+    fn remove(&mut self, value: Identifier);
+    fn threshold(&self) -> usize;
+    fn count(&self) -> u16;
+}
