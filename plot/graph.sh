472e703aff6fa05cb61b2e85c80db800b28c3a0c
Gina Yuan <gina.yuan@gmail.com>
2023-02-09 23:51:07 +0000

    plotting

--- plot/graph.sh
+++ plot/graph.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-execute=$1
-python3 baseline_bar.py -t 10 $execute
-python3 data_size_vs_total_time.py --mean $execute -t 10
-python3 loss_vs_tput.py --max-x 800 -n 20M -t 5 $execute
-python3 time_vs_cwnd.py --max-x 30 --quic-n 40M --quack-n 40M --tcp 70 --loss 0 $execute
-python3 time_vs_cwnd.py --max-x 30 --quic-n 20M --quack-n 70M --tcp 70 --loss 5 $execute
-python3 multiflow.py -n 15M $execute loss5p
-python3 multiflow.py -n 30M $execute loss0p
+trials=$1
+execute=$2
+python3 baseline_bar.py -t $1 $execute
+python3 data_size_vs_total_time.py --mean --median -t $1 $execute
+python3 loss_vs_tput.py --max-x 800 -n 25M -t $1 $execute
+#python3 time_vs_cwnd.py --max-x 30 --quic-n 35M --quack-n 35M --tcp 35 --loss 0 $execute
+#python3 time_vs_cwnd.py --max-x 30 --quic-n 5M --quack-n 35M --tcp 35 --loss 1 $execute
+#python3 multiflow.py -n 60M --max-x 60 $execute loss1p
+#python3 multiflow.py -n 60M --max-x 60 $execute loss0p
 
