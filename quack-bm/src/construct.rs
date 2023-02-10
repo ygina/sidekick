48bd150a40e68e9f7b8a0ac28f295627ebcbc8ca
Gina Yuan <gina.yuan@gmail.com>
2023-02-10 02:25:24 +0000

    quack bming

--- quack-bm/src/construct.rs
+++ quack-bm/src/construct.rs
@@ -1,56 +1,75 @@
 use crate::common::*;
 
-use std::ops::{Sub, SubAssign};
 use std::time::{Instant, Duration};
 use log::info;
 use rand::Rng;
 use quack::*;
+use multiset::HashMultiSet;
 
-fn benchmark_construct_32<T: Quack + Sub + SubAssign>(
+fn benchmark_construct_strawman1(
+    numbers: Vec<u32>,
+    num_packets: usize,
+    num_drop: usize,
+) -> Duration {
+    let mut acc1 = HashMultiSet::new();
+    let mut acc2 = HashMultiSet::new();
+
+    // Warm up the instruction cache by inserting a few numbers.
+    for i in num_packets..(num_packets + 10) {
+        acc1.insert(numbers[i]);
+    }
+    for i in num_packets..(num_packets + 10) {
+        acc2.insert(numbers[i]);
+    }
+
+    // Insert a bunch of random numbers into the accumulator.
+    let t1 = Instant::now();
+    for j in 0..num_packets {
+        acc1.insert(numbers[j]);
+    }
+    for j in 0..(num_packets - num_drop) {
+        acc2.insert(numbers[j]);
+    }
+    let t2 = Instant::now();
+
+    let duration = t2 - t1;
+    info!("Insert {} numbers into 2 multisets: {:?}",
+        num_packets, duration);
+    duration
+}
+
+fn benchmark_construct_power_sum_32(
+    numbers: Vec<u32>,
     size: usize,
     num_packets: usize,
     num_drop: usize,
-    num_trials: usize,
-) {
-    let mut rng = rand::thread_rng();
+) -> Duration {
+    // Construct two empty Quacks.
+    let mut acc1 = PowerSumQuack::new(size);
+    let mut acc2 = PowerSumQuack::new(size);
 
-    // Allocate buffer for benchmark durations.
-    let mut durations: Vec<Duration> = vec![];
-
-    for i in 0..(num_trials + 1) {
-        let numbers: Vec<u32> =
-            (0..(num_packets + 10)).map(|_| rng.gen()).collect();
-
-        // Construct two empty Quacks.
-        let mut acc1 = T::new(size);
-        let mut acc2 = T::new(size);
-
-        // Warm up the instruction cache by inserting a few numbers.
-        for i in num_packets..(num_packets + 10) {
-            acc1.insert(numbers[i]);
-        }
-        for i in num_packets..(num_packets + 10) {
-            acc2.insert(numbers[i]);
-        }
-
-        // Insert a bunch of random numbers into the accumulator.
-        let t1 = Instant::now();
-        for j in 0..num_packets {
-            acc1.insert(numbers[j]);
-        }
-        for j in 0..(num_packets - num_drop) {
-            acc2.insert(numbers[j]);
-        }
-        let t2 = Instant::now();
-
-        if i > 0 {
-            let duration = t2 - t1;
-            info!("Insert {} numbers into 2 Quacks (u32, \
-                threshold = {}): {:?}", num_packets, size, duration);
-            durations.push(duration);
-        }
+    // Warm up the instruction cache by inserting a few numbers.
+    for i in num_packets..(num_packets + 10) {
+        acc1.insert(numbers[i]);
     }
-    print_summary(durations);
+    for i in num_packets..(num_packets + 10) {
+        acc2.insert(numbers[i]);
+    }
+
+    // Insert a bunch of random numbers into the accumulator.
+    let t1 = Instant::now();
+    for j in 0..num_packets {
+        acc1.insert(numbers[j]);
+    }
+    for j in 0..(num_packets - num_drop) {
+        acc2.insert(numbers[j]);
+    }
+    let t2 = Instant::now();
+
+    let duration = t2 - t1;
+    info!("Insert {} numbers into 2 Quacks (u32, \
+        threshold = {}): {:?}", num_packets, size, duration);
+    duration
 }
 
 pub fn run_benchmark(
@@ -64,12 +83,27 @@ pub fn run_benchmark(
 ) {
     assert!(!use_tables, "ERROR: power tables are not enabled");
     assert_eq!(num_bits_id, 32, "ERROR: <num_bits_id> must be 32");
-    match quack_ty {
-        QuackType::Strawman1 => unimplemented!(),
-        QuackType::Strawman2 => unimplemented!(),
-        QuackType::PowerSum => benchmark_construct_32::<PowerSumQuack>(
-            threshold, num_packets, num_drop, num_trials),
-        QuackType::Montgomery => benchmark_construct_32::<MontgomeryQuack>(
-            threshold, num_packets, num_drop, num_trials),
+
+    let mut rng = rand::thread_rng();
+
+    // Allocate buffer for benchmark durations.
+    let mut durations: Vec<Duration> = vec![];
+
+    for i in 0..(num_trials + 1) {
+        let numbers: Vec<u32> =
+            (0..(num_packets + 10)).map(|_| rng.gen()).collect();
+
+        let duration = match quack_ty {
+            QuackType::Strawman1 => benchmark_construct_strawman1(
+                numbers, num_packets, num_drop),
+            QuackType::Strawman2 => unimplemented!(),
+            QuackType::PowerSum => benchmark_construct_power_sum_32(
+                numbers, threshold, num_packets, num_drop),
+            QuackType::Montgomery => unimplemented!(),
+        };
+        if i > 0 {
+            durations.push(duration);
+        }
     }
+    print_summary(durations);
 }
--- quack-bm/src/decode.rs
+++ quack-bm/src/decode.rs
@@ -1,17 +1,60 @@
 use crate::common::*;
 
-use std::ops::{Sub, SubAssign};
 use std::time::{Instant, Duration};
 use log::info;
 use rand::Rng;
 use quack::*;
 
-fn benchmark_decode_32<T: Quack + Sub + SubAssign>(
+fn benchmark_decode_power_sum_32(
+    numbers: Vec<u32>,
+    factor: bool,
     size: usize,
     num_packets: usize,
     num_drop: usize,
+) -> Duration {
+    // Construct two empty Quacks.
+    let mut acc1 = PowerSumQuack::new(size);
+    let mut acc2 = PowerSumQuack::new(size);
+
+    // Insert all random numbers into the first accumulator.
+    for j in 0..num_packets {
+        acc1.insert(numbers[j]);
+    }
+
+    // Insert all but num_drop random numbers into the second accumulator.
+    for j in 0..(num_packets - num_drop) {
+        acc2.insert(numbers[j]);
+    }
+
+    let t1 = Instant::now();
+    acc1 -= acc2;
+    let dropped = if factor {
+        acc1.decode_by_factorization().unwrap()
+    } else {
+        acc1.decode_with_log(&numbers)
+    };
+    let t2 = Instant::now();
+
+    let duration = t2 - t1;
+    info!("Decode time (u32, threshold = {}, num_packets={}, \
+        false_positives = {}, dropped = {}): {:?}", size, num_packets,
+        dropped.len() - num_drop, num_drop, duration);
+    duration
+}
+
+pub fn run_benchmark(
+    quack_ty: QuackType,
+    use_tables: bool,
+    factor: bool,
+    threshold: usize,
+    num_packets: usize,
+    num_bits_id: usize,
+    num_drop: usize,
     num_trials: usize,
 ) {
+    assert!(!use_tables, "ERROR: power tables are not enabled");
+    assert_eq!(num_bits_id, 32, "ERROR: <num_bits_id> must be 32");
+
     let mut rng = rand::thread_rng();
 
     // Allocate buffer for benchmark durations.
@@ -22,55 +65,16 @@ fn benchmark_decode_32<T: Quack + Sub + SubAssign>(
         let numbers: IdentifierLog =
             (0..num_packets).map(|_| rng.gen()).collect();
 
-        // Construct two empty Quacks.
-        let mut acc1 = T::new(size);
-        let mut acc2 = T::new(size);
-
-        // Insert all random numbers into the first accumulator.
-        for j in 0..num_packets {
-            acc1.insert(numbers[j]);
-        }
-
-        // Insert all but num_drop random numbers into the second accumulator.
-        for j in 0..(num_packets - num_drop) {
-            acc2.insert(numbers[j]);
-        }
-
-        let t1 = Instant::now();
-        acc1 -= acc2;
-        let dropped = acc1.decode(&numbers);
-        // do_not_discard(dropped);
-        let t2 = Instant::now();
-
+        let duration = match quack_ty {
+            QuackType::Strawman1 => unimplemented!(),
+            QuackType::Strawman2 => unimplemented!(),
+            QuackType::PowerSum => benchmark_decode_power_sum_32(
+                numbers, factor, threshold, num_packets, num_drop),
+            QuackType::Montgomery => unimplemented!(),
+        };
         if i > 0 {
-            let duration = t2 - t1;
-            info!("Decode time (u32, threshold = {}, num_packets={}, \
-                false_positives = {}, dropped = {}): {:?}", size, num_packets,
-                dropped.len() - num_drop, num_drop, duration);
             durations.push(duration);
         }
     }
-
     print_summary(durations);
-}
-
-pub fn run_benchmark(
-    quack_ty: QuackType,
-    use_tables: bool,
-    threshold: usize,
-    num_packets: usize,
-    num_bits_id: usize,
-    num_drop: usize,
-    num_trials: usize,
-) {
-    assert!(!use_tables, "ERROR: power tables are not enabled");
-    assert_eq!(num_bits_id, 32, "ERROR: <num_bits_id> must be 32");
-    match quack_ty {
-        QuackType::Strawman1 => unimplemented!(),
-        QuackType::Strawman2 => unimplemented!(),
-        QuackType::PowerSum => benchmark_decode_32::<PowerSumQuack>(
-            threshold, num_packets, num_drop, num_trials),
-        QuackType::Montgomery => benchmark_decode_32::<MontgomeryQuack>(
-            threshold, num_packets, num_drop, num_trials),
-    }
 }
