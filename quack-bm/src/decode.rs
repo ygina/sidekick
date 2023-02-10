9c0613d1f05a945593416fea107fdaf9639ad243
Gina Yuan <gina.yuan@gmail.com>
2023-02-10 04:27:19 +0000

    strawman2

--- quack-bm/src/decode.rs
+++ quack-bm/src/decode.rs
@@ -5,6 +5,7 @@ use log::info;
 use rand::Rng;
 use quack::*;
 use multiset::HashMultiSet;
+use sha2::{Digest, Sha256};
 
 fn benchmark_decode_strawman1(
     numbers: Vec<u32>,
@@ -34,6 +35,54 @@ fn benchmark_decode_strawman1(
         false_positives = {}, dropped = {}): {:?}", num_packets,
         dropped.len() - num_drop, num_drop, duration);
     assert_eq!(dropped.len(), num_drop);
+    duration
+}
+
+const NUM_SUBSETS_LIMIT: u32 = 1000000;
+
+fn benchmark_decode_strawman2(
+    numbers: Vec<u32>,
+    num_packets: usize,
+    num_drop: usize,
+) -> Duration {
+    let mut acc1 = Sha256::new();
+
+    // Insert all but num_drop random numbers into the accumulator.
+    for i in 0..(num_packets - num_drop) {
+        acc1.update(numbers[i].to_be_bytes());
+    }
+    acc1.finalize();
+
+    // Calculate the number of subsets.
+    let n = num_packets as u32;
+    let r = num_drop as u32;
+    // let num_subsets = (n-r+1..=n).product();
+
+    let t1 = Instant::now();
+    if num_drop > 0 {
+        // For every subset of size "num_packets - num_drop"
+        // Calculate the SHA256 hash
+        // let num_hashes_to_calculate = std::cmp::min(
+        //     NUM_SUBSETS_LIMIT, num_subsets / 2);
+        let num_hashes_to_calculate = NUM_SUBSETS_LIMIT;
+
+        // We're really just measuring a lower bound of the time to compute
+        // any SHA256 hash with this number of elements
+        for _ in 0..num_hashes_to_calculate {
+            let mut acc2 = Sha256::new();
+            for j in 0..(num_packets - num_drop) {
+                acc2.update(numbers[j].to_be_bytes());
+            }
+            acc2.finalize();
+        }
+    }
+    let t2 = Instant::now();
+
+    let duration = t2 - t1;
+    info!("Decode time (num_packets={}): {:?}", num_packets, duration);
+    info!("Calculated {} hashes, expected {}C{}",
+        NUM_SUBSETS_LIMIT, num_packets, num_drop);
+
     duration
 }
 
@@ -101,7 +150,8 @@ pub fn run_benchmark(
         let duration = match quack_ty {
             QuackType::Strawman1 => benchmark_decode_strawman1(
                 numbers, num_packets, num_drop),
-            QuackType::Strawman2 => unimplemented!(),
+            QuackType::Strawman2 => benchmark_decode_strawman2(
+                numbers, num_packets, num_drop),
             QuackType::PowerSum => benchmark_decode_power_sum_32(
                 numbers, factor, threshold, num_packets, num_drop),
             QuackType::Montgomery => unimplemented!(),
