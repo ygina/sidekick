7b9e13a307fd12ee4eda14db038d6e04241b323e
Gina Yuan <gina.yuan@gmail.com>
2023-06-28 13:22:33 -0700

    visualize frames one at a time

--- visualizer/javascript/index.js
+++ visualizer/javascript/index.js
@@ -1,8 +1,18 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
 
+var data = [];
+
+class Match {
+  constructor(instant, sidecarId, reason) {
+    this.instant = instant;
+    this.sidecarId = sidecarId;
+    this.reason = reason;
+  }
+}
+
 // Parse all lines that begin with quack_log and return an array of arrays
-// [seconds_since_start, sidecar_id, reason]. Possible reasons include: sent,
-// quacked, acked, detect_lost_packets, sidecar_detect_lost_packets.
+// [secondsSinceStart, sidecarId, reasonInt]. Possible reasons include:
+// sent, quacked, acked, detect_lost_packets, sidecar_detect_lost_packets.
 async function parseFile(file) {
   const text = (await file.text()).split('\n')
   const re = /quack_log Instant { tv_sec: (\d+), tv_nsec: (\d+) } (\d+) \((\S+)\).*/
@@ -23,14 +33,51 @@ async function parseFile(file) {
   }).map(function(match) {
     if (!minTime)
       minTime = match[0]
-    match[0] -= minTime
-    return match;
+    return new Match(match[0] - minTime, match[1], match[2]);
   })
 
   return data;
 }
 
+// Creates a span element for the given reason.
+function createSpan(sidecarId) {
+  const span = document.createElement("span");
+  span.id = sidecarId;
+  span.alt = sidecarId;
+  span.innerHTML = "0";
+  span.classList.add("box");
+  span.classList.add("sent");
+  return span;
+}
+
+function applyFrame(index) {
+  if (index >= data.length) {
+    console.error('index outside of frame length');
+    return;
+  }
+  const frame = data[index];
+  document.getElementById('timeSinceStart').innerHTML = frame.instant
+  if (frame.reason == "sent") {
+    const span = createSpan(frame.sidecarId);
+    const container = document.getElementById('container');
+    container.appendChild(span);
+    container.appendChild(document.createTextNode(' '))
+  } else {
+    document.getElementById(frame.sidecarId).classList.add(frame.reason)
+  }
+}
+
 document.getElementById('myFile').onchange = async function(a, b, c) {
-  const data = await parseFile(this.files[0]);
+  data = await parseFile(this.files[0]);
+  document.getElementById('maxFrames').innerHTML = data.length;
+  document.getElementById('container').innerHTML = '';
   console.log(data);
+  applyFrame(0);
+}
+
+document.getElementById('forward-button').onclick = function() {
+  const frameInput = document.getElementById('frameNumber');
+  const index = parseInt(frameInput.value) + 1;
+  frameInput.value = index;
+  applyFrame(index);
 }
