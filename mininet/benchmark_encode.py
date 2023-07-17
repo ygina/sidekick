23ed6108086336447acf153aa2209d2c2da32fae
Gina Yuan <gina.yuan@gmail.com>
2023-07-17 17:05:56 +0000

    refactor python benchmark script for measuring multiple quacks at once

--- mininet/benchmark_encode.py
+++ mininet/benchmark_encode.py
@@ -4,7 +4,7 @@ import time
 from network import *
 from mininet.log import setLogLevel
 
-def start_iperf_servers(net, count=2):
+def start_iperf_servers(net, count):
     servers = []
     for i in range(count):
         cmd = f'taskset -c 0 iperf3 -s -f m -p 520{i+1}'.split(' ')
@@ -13,7 +13,7 @@ def start_iperf_servers(net, count=2):
         servers.append(p)
     return servers
 
-def start_iperf_clients(net, count=2):
+def start_iperf_clients(net, count):
     clients = []
     cmd = ['iperf3', '-c', '10.0.1.10', '--udp', '--congestion', 'cubic']
     cmd += ['--time', str(args.warmup + args.timeout + 1)]
@@ -27,35 +27,30 @@ def start_iperf_clients(net, count=2):
         clients.append(p)
     return clients
 
-if __name__ == '__main__':
-    setLogLevel('info')
+def start_iperf(net, count=2):
+    servers = start_iperf_servers(net, count)
+    time.sleep(1)
+    clients = start_iperf_clients(net, count)
+    return (servers, clients)
 
-    parser = argparse.ArgumentParser(prog='Benchmark Encoder')
+def print_loadgen_output(servers, clients):
+    for server in servers:
+        sys.stdout.buffer.write(server.stdout.peek())
+        sys.stdout.buffer.write(b'\n')
+    for client in clients:
+        sys.stdout.buffer.write(client.stdout.peek())
+        sys.stdout.buffer.write(b'\n')
+    sys.stdout.buffer.flush()
 
-    ############################################################################
-    # Network Configurations
-    parser.add_argument('--tput', default=500, type=int, metavar='PPS',
-        help='Target load generator throughput in packets per second. The load '
-             'generator may not be able to achieve too high of throughputs. '
-             '(default: 500)')
-    parser.add_argument('--length', '-l', default=70, type=int, metavar='BYTES',
-        help='Target load generator packet length, the -l option in iperf3 '
-             '(default: 70)')
-    parser.add_argument('--warmup', '-w', type=int, default=3, metavar='S',
-        help='Warmup time, in seconds (default: 3)')
-    parser.add_argument('--timeout', '-t', type=int, default=5, metavar='S',
-        help='Timeout, in seconds (default: 5)')
-    parser.add_argument('--frequency', type=int, default=0,
-        help='Quack frequency in ms (default: 0)')
-    parser.add_argument('--threshold', type=int, default=20, metavar='PACKETS',
-        help='Quack threshold (default: 20)')
-    parser.add_argument('--disable-sidecar', action='store_true',
-        help='Disable the sidecar to test only iperf load generator')
+def print_sidecar_output(sidecar):
+    for line in sidecar.stdout.peek().split(b'\n'):
+        if b'DONE' in line:
+            break
+        sys.stdout.buffer.write(line)
+        sys.stdout.buffer.write(b'\n')
+        sys.stdout.buffer.flush()
 
-    args = parser.parse_args()
-    net = SidecarNetwork(delay1=0, delay2=0, loss1=0, loss2=0, bw1=0,
-        bw2=0, qdisc='none')
-
+def run_benchmark_single(net, args):
     """
     1. Set up the network
     2. Start the load generator at the given rate on h2.
@@ -70,33 +65,70 @@ if __name__ == '__main__':
         * Load generator: target tput (packets/s); tput (packets/s)
     """
     # load_generator = net.h2.popen(f'./target/release/load_generator --warmup {args.warmup} --tput {args.tput}'.split(' '))
-    servers = start_iperf_servers(net)
-    time.sleep(1)
-    clients = start_iperf_clients(net)
+    servers, clients = start_iperf(net)
     time.sleep(args.warmup)
-    if not args.disable_sidecar:
+    if args.disable_sidecar:
+        time.sleep(args.timeout)
+        print_loadgen_output(servers, clients)
+    else:
         env = os.environ.copy()
         # env['RUST_LOG'] = 'trace'
         r1 = net.r1.popen(f'taskset -c 3 ./target/release/benchmark_encode --threshold {args.threshold} --frequency {args.frequency}'.split(' '),
             stderr=subprocess.STDOUT, stdout=subprocess.PIPE, env=env)
-    time.sleep(args.timeout)
-    if not args.disable_sidecar:
+        time.sleep(args.timeout)
         r1.terminate()
-    sys.stdout.buffer.write(servers[0].stdout.peek())
-    sys.stdout.buffer.write(b'\n')
-    sys.stdout.buffer.write(servers[1].stdout.peek())
-    sys.stdout.buffer.write(b'\n')
-    sys.stdout.buffer.write(clients[0].stdout.peek())
-    sys.stdout.buffer.write(b'\n')
-    sys.stdout.buffer.write(clients[1].stdout.peek())
-    sys.stdout.buffer.write(b'\n')
-    sys.stdout.buffer.flush()
-
-    if not args.disable_sidecar:
-        for line in r1.stdout.peek().split(b'\n'):
-            if b'DONE' in line:
-                break
-            sys.stdout.buffer.write(line)
-            sys.stdout.buffer.write(b'\n')
-            sys.stdout.buffer.flush()
+        print_loadgen_output(servers, clients)
+        print_sidecar_output(r1)
     net.stop()
+
+def run_benchmark_multi(net, args):
+    pass
+
+if __name__ == '__main__':
+    setLogLevel('info')
+
+    parser = argparse.ArgumentParser(prog='Benchmark Encoder')
+    subparsers = parser.add_subparsers(required=True)
+
+    ############################################################################
+    # Network Configurations
+    parser.add_argument('--warmup', '-w', type=int, default=3, metavar='S',
+        help='Warmup time, in seconds (default: 3)')
+    parser.add_argument('--timeout', '-t', type=int, default=5, metavar='S',
+        help='Timeout, in seconds (default: 5)')
+    parser.add_argument('--disable-sidecar', action='store_true',
+        help='Disable the sidecar to test only iperf load generator')
+
+    ############################################################################
+    # Load generator configurations
+    loadgen_config = parser.add_argument_group('loadgen_config')
+    loadgen_config.add_argument('--tput', default=500, type=int, metavar='PPS',
+        help='Target load generator throughput in packets per second for each '
+             'iperf client. The load generator may not be able to achieve too '
+             'high of throughputs. (default: 500)')
+    loadgen_config.add_argument('--length', '-l', default=70, type=int, metavar='BYTES',
+        help='Target load generator packet length, the -l option in iperf3 '
+             '(default: 70)')
+
+    ############################################################################
+    # Sidecar configurations
+    sidecar_config = parser.add_argument_group('sidecar_config')
+    sidecar_config.add_argument('--frequency', type=int, default=0,
+        help='Quack frequency in ms (default: 0)')
+    sidecar_config.add_argument('--threshold', type=int, default=20, metavar='PACKETS',
+        help='Quack threshold (default: 20)')
+
+    ############################################################################
+    # Single quack
+    single = subparsers.add_parser('single')
+    single.set_defaults(benchmark=run_benchmark_single)
+
+    ############################################################################
+    # Multiple quacks
+    multi = subparsers.add_parser('multi')
+    multi.set_defaults(benchmark=run_benchmark_multi)
+
+    args = parser.parse_args()
+    net = SidecarNetwork(delay1=0, delay2=0, loss1=0, loss2=0, bw1=0,
+        bw2=0, qdisc='none')
+    args.benchmark(net, args)
