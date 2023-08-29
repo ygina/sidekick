dc9f01f1672cade4ae339a7cc1f6db96106b9569
Gina Yuan <gina.yuan@gmail.com>
2023-08-29 01:18:22 +0000

    install dependencies for plotting scripts

--- deps/install_deps.sh
+++ deps/install_deps.sh
@@ -25,6 +25,15 @@ sudo apt-get install -y python3-virtualenv  # plotting
 # mininet
 pip3 install mininet
 
+# plotting scripts
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
+cd $SIDECAR_HOME/plot
+virtualenv -p python3 env
+source env/bin/activate
+pip install -r requirements.txt
+echo "source $HOME/sidecar/plot/env/bin/activate" >> ~/.bashrc
+
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  # hit 1
 source "$HOME/.cargo/env"
