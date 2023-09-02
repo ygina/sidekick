0ac4bb6a91db9aad4496cea3952496831dca9e8d
Gina Yuan <gina.yuan@gmail.com>
2023-09-01 18:51:43 -0700

    add log level and sudo to quack senders

--- scripts/retx/start_sender.sh
+++ scripts/retx/start_sender.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$HOME/sidecar/target/release/sender -i wlp1s0 -t 10 --target-addr 10.42.0.178:5103 --frequency-ms 30 --my-addr 10.42.0.1
+sudo RUST_LOG=info $HOME/sidecar/target/release/sender -i wlp1s0 -t 10 --target-addr 10.42.0.178:5103 --frequency-ms 30 --my-addr 10.42.0.1
 
--- scripts/webrtc/start_sender.sh
+++ scripts/webrtc/start_sender.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$HOME/sidecar/target/release/sender -i wlp1s0 -t 8 --target-addr 10.42.0.178:5103 --frequency-pkts 2 --my-addr 10.42.0.1
+sudo RUST_LOG=info $HOME/sidecar/target/release/sender -i wlp1s0 -t 8 --target-addr 10.42.0.178:5103 --frequency-pkts 2 --my-addr 10.42.0.1
 
