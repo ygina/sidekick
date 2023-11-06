e05b3961307ff359a6e9361353932cc6d3396622
Gina Yuan <gina.yuan@gmail.com>
2023-11-06 18:42:17 +0000

    update webrtc start server script

--- scripts/webrtc/start_server.sh
+++ scripts/webrtc/start_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+	echo "USAGE: $0 [OUTPUT_FILE]"
+	exit 1
+fi
+
 export RUST_LOG=debug
-$HOME/sidecar/target/release/webrtc_server --port 5201 --rtt 110 --loop
+$HOME/sidecar/target/release/webrtc_server --port 5201 --rtt 110 --loop 2>&1 | tee -a $1
 
